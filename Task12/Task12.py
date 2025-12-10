6e350360a29d9dae22bd0ed4dd113eaedddbadb9
Maxiur <mateusz222244@op.pl>
2025-12-10 21:12:06 +0100

    done

--- Task12/Task12.py
+++ Task12/Task12.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.typing import NDArray
+from scipy.special import comb
 
 vector = NDArray[np.float64]
 matrix = NDArray[np.float64]
@@ -12,19 +13,36 @@ def f_x(x: num) -> num:
 def floater_hormann_weights(x: vector, d: int) -> vector:
     n = len(x)
     w = np.zeros(n, dtype=num)
+
     for k in range(n):
         start = max(0, k - d)
         end = min(k, n - d - 1)
+
         s = 0.0
         for i in range(start, end + 1):
-            sign = (-1) ** i
-            prod = 1.0
-            for j in range(i, i + d + 1):
-                if j != k:
-                    prod *= 1.0 / (x[k] - x[j])
-            s += sign * prod
-        w[k] = s
+            s += comb(d, k - i, exact=True)
+
+        sign = 1 if (k - d) % 2 == 0 else -1
+        w[k] = sign * s
+
     return w
+
+def floater_hormann_function(x_nodes: vector, f:vector, w: vector, x_eval: vector) -> vector:
+    r = np.zeros_like(x_eval)
+    for idx, x in enumerate(x_eval):
+        diff = x - x_nodes
+
+        mask = np.isclose(diff, 0.0, atol=1e-12)
+        if np.any(mask):
+            r[idx] = f[mask][0]
+        else:
+            w_diff = w / diff
+            numerator = np.sum(w_diff * f)
+            denominator = np.sum(w_diff)
+            r[idx] = numerator / denominator
+
+    return r
+
 
 def main():
     x: vector = np.array([-7/8, -5/8, -3/8, -1/8, 1/8, 3/8, 5/8, 7/8], dtype=num)
@@ -33,6 +51,19 @@ def main():
 
     for i in range(n):
         f[i] = f_x(x[i])
+
+    d = 3
+    w = floater_hormann_weights(x, d)
+    print(w)
+
+    x_eval = np.linspace(-1.25, 1.25, 400)
+    y_eval = floater_hormann_function(x, f, w, x_eval)
+
+    plt.plot(x_eval, y_eval, label="Interpolacja Floatera–Hormanna (d=3)")
+    plt.plot(x, f, 'ro', label="Węzły")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
