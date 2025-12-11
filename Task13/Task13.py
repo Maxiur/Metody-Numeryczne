79cca8f756ea1999b78ff678686240ef8ef08211
Mateusz Kamiński <maxiur@Mateuszs-MacBook-Air.local>
2025-12-11 14:26:38 +0100

    Init

---
+++ Task13/Task13.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy.typing import NDArray
+
+vector = NDArray[np.float64]
+matrix = NDArray[np.float64]
+num = np.float64
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
