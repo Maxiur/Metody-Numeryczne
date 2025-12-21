9cb252ac6e866446cb90f1f1d8eacb29a803db6a
Maxiur <maxiur25@gmail.com>
2025-12-21 12:59:47 +0100

    init

---
+++ Task14/Task14.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.typing import NDArray
+
+vector = NDArray[np.float64]
+matrix = NDArray[np.float64]
+num = np.float64
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
